0db4f1f441761b67447588c56377a6c7b62a72c7
Ronny Peña <ra.pp.91.15@gmail.com>
2022-05-08 21:58:55 +0200

    ActivityMain: añadido el Bottom Navigation y los fragment basico

--- app/src/main/java/com/example/onfieldtbs_android/MainActivity.java
+++ app/src/main/java/com/example/onfieldtbs_android/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.onfieldtbs_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.onfieldtbs_android.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.navView);
+
+         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragmentMain);
+         NavController navController = navHostFragment.getNavController();
+
+         NavigationUI.setupWithNavController(navView, navController);
     }
+
 }
