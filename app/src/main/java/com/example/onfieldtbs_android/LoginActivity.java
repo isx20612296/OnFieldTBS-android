801004d1e3fb68ae8d408f2ec69b55ea7c14dccc
andreupasalamar <andreupas23@gmail.com>
2022-05-09 11:24:54 +0200

    Login Email cambiado a Username

--- app/src/main/java/com/example/onfieldtbs_android/LoginActivity.java
+++ app/src/main/java/com/example/onfieldtbs_android/LoginActivity.java
@@ -12,13 +12,11 @@ import com.example.onfieldtbs_android.databinding.ActivityLoginBinding;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String ERR_EMPTY = "Error, campos vacíos";
-    private static final String ERR_FORMAT = "Error, correo electrónico no válido";
     private static final String ERR_PASS = "Error, la contraseña es demasiado corta";
-    private static final String FORMAT_EMAIL = "^[^@].*@oftbs.org$";
     private static final int MIN_LENGTH = 8;
 
     private ActivityLoginBinding binding;
-    private String email;
+    private String username;
     private String password;
     private Intent mainIntent;
 
@@ -29,27 +27,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // Set initial text
-        binding.loginEmailEditText.setText("");
+        binding.loginUsernameEditText.setText("");
         binding.loginPasswordEditText.setText("");
 
         // Button Listener
         binding.loginButton.setOnClickListener(view -> {
 
             // Check if empty
-            if (binding.loginEmailEditText.getText().toString().matches("") || binding.loginPasswordEditText.getText().toString().matches("")){
+            if (binding.loginUsernameEditText.getText().toString().matches("") || binding.loginPasswordEditText.getText().toString().matches("")){
                 makeToast(ERR_EMPTY);
                 return;
             }
 
             // Get email and password
-            email = binding.loginEmailEditText.getText().toString();
+            username = binding.loginUsernameEditText.getText().toString();
             password = binding.loginPasswordEditText.getText().toString();
-
-            // Check email format
-            if (!email.matches(FORMAT_EMAIL)){
-                makeToast(ERR_FORMAT);
-                return;
-            }
 
             // Check password length
             if(password.length() < MIN_LENGTH){
@@ -59,7 +51,7 @@ public class LoginActivity extends AppCompatActivity {
 
             // TEST
             // Show email and password in Debug Log
-            Log.d("LOGIN", "email: " + email + ", password: " + password);
+            Log.d("LOGIN", "username: " + username + ", password: " + password);
 
             // Go to Main Activity
             mainIntent = new Intent(LoginActivity.this, MainActivity.class);
