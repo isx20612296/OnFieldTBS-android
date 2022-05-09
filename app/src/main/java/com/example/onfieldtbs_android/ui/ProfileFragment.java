b2bde73f58f5442772b3d6bdd24a4abf07e4291b
andreupasalamar <andreupas23@gmail.com>
2022-05-09 11:17:26 +0200

    Fragment Profile diseño básico

--- app/src/main/java/com/example/onfieldtbs_android/ui/ProfileFragment.java
+++ app/src/main/java/com/example/onfieldtbs_android/ui/ProfileFragment.java
@@ -9,21 +9,32 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.onfieldtbs_android.R;
+import com.example.onfieldtbs_android.databinding.FragmentProfileBinding;
 
 
 public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        //return inflater.inflate(R.layout.fragment_profile, container, false);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
