cabd9602bbce371b86b12fd296856259d1cf9657
andreupasalamar <andreupas23@gmail.com>
2022-05-09 19:52:28 +0200

    Incidence Fragment Spinners

--- app/src/main/java/com/example/onfieldtbs_android/ui/IncidenceFragment.java
+++ app/src/main/java/com/example/onfieldtbs_android/ui/IncidenceFragment.java
@@ -1,29 +1,115 @@
 package com.example.onfieldtbs_android.ui;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.onfieldtbs_android.MainActivity;
 import com.example.onfieldtbs_android.R;
+import com.example.onfieldtbs_android.databinding.ActivityLoginBinding;
+import com.example.onfieldtbs_android.databinding.FragmentIncidenceBinding;
+import com.example.onfieldtbs_android.databinding.FragmentProfileBinding;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class IncidenceFragment extends Fragment {
 
+    private FragmentIncidenceBinding binding;
+
+    private String [] states;
+    private String [] priorities;
+    private String [] datesSorting;
+    private ArrayAdapter<String> statesAdapter;
+    private ArrayAdapter<String> prioritiesAdapter;
+    private ArrayAdapter<String> datesSortingAdapter;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentIncidenceBinding.inflate(inflater, container, false);
+
+        // Set lists data
+        states = new String[]{"Estado", "Abierto", "En progreso", "Pausado", "Cerrado"};
+        priorities = new String[] {"Prioridad", "Alta", "Media", "Baja"};
+        datesSorting = new String[]{"Fecha", "Recientes", "Antiguos"};
+
+        // Create Spinner adapters
+        statesAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_list, states);
+        prioritiesAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_list, priorities);
+        datesSortingAdapter = new ArrayAdapter<>(getContext(), R.layout.spinner_list, datesSorting);
+
+        // Set adapters to Spinners
+        binding.incidenceStateSpinner.setAdapter(statesAdapter);
+        binding.incidencePrioritySpinner.setAdapter(prioritiesAdapter);
+        binding.incidenceDateSpinner.setAdapter(datesSortingAdapter);
+
+        // Set default values
+        binding.incidenceStateSpinner.setSelection(0);
+        binding.incidencePrioritySpinner.setSelection(0);
+        binding.incidenceDateSpinner.setSelection(0);
+
+        // Set actions to do when item selected
+        binding.incidenceStateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("INCIDENCE", "Selected: " + adapterView.getItemAtPosition(i).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("INCIDENCE", "Selected: NONE");
+            }
+        });
+
+        binding.incidencePrioritySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("INCIDENCE", "Selected: " + adapterView.getItemAtPosition(i).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("INCIDENCE", "Selected: NONE");
+            }
+        });
+        binding.incidenceDateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("INCIDENCE", "Selected: " + adapterView.getItemAtPosition(i).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("INCIDENCE", "Selected: NONE");
+            }
+        });
+
+        return binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_incidence, container, false);
+        // return inflater.inflate(R.layout.fragment_incidence, container, false);
     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
 }
